72bad9bdd615537215dc5da87fcc53ccb2320ebe
Tatiana <tatianaoe83@gmail.com>
2025-04-01 16:51:45 -0600

    Update fields.blade.php

--- resources/views/puestos/fields.blade.php
+++ resources/views/puestos/fields.blade.php
@@ -8,8 +8,10 @@
 <div class="form-group col-sm-6">
     {!! Form::label('DepartamentoID', 'Departamento:') !!}
 
-    {!!Form::select('DepartamentoID',App\Models\Departamentos::all()->
-        pluck('NombreDepartamento','DepartamentoID'),null,['placeholder' => 'Seleccionar','class'=>'jz form-control'])!!}
+    {!!Form::select('DepartamentoID',App\Models\Departamentos::select(DB::raw("CONCAT(departamentos.NombreDepartamento,' - ', gerencia.NombreGerencia) AS NombreDepartamento, departamentos.DepartamentoID"))
+        ->join('gerencia', 'gerencia.GerenciaID', '=', 'departamentos.GerenciaID')
+        ->pluck('NombreDepartamento','DepartamentoID'),null,[ 'style' => 'width: 100%','placeholder' => 'SELECCIONAR','class'=>'form-control jz'])!!}
+
 
    
 </div>
